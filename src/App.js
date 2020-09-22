cd8d8d0bf19e45cc9334e97ebc000882dbf6428f
kker <907765631@qq.com>
2020-09-22 17:25:33 +0800

    [Mamingjun]: implement HTML

--- src/App.js
+++ src/App.js
@@ -1,9 +1,58 @@
 import React, { Component } from 'react';
 import './App.scss';
+import avatar from './assets/avatar.jpg';
 
 class App extends Component {
   render() {
-    return <main className="app">start here</main>;
+    return (
+      <main className="app">
+        <img src={avatar} alt="头像" />
+        <h1>HELLO,</h1>
+        <h2>MY NAME IS KAMIL 24YO AND THIS IS MY RESUME/CV</h2>
+        <h3>ABOUT ME</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus,
+          non, dolorem, cumque distinctio magni quam expedita veit laborum sunt
+          amet facere tempora ut fuga aliquam ad asperiores voluptatem dolorum!
+          Quasi.
+        </p>
+        <h3>EDUCATION</h3>
+        <section>
+          <h4>1990</h4>
+          <article>
+            <h4>I was born in Katowice</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Sapiente, exercitationem, totam, dolores iste dolore est aut modi.
+            </p>
+          </article>
+        </section>
+        <section>
+          <h4>2005</h4>
+          <article>
+            <h4>Secondary school specializing in artistic</h4>
+            <p>
+              Eos, explicato, nam, tenetur et ab eius deserunt aspernatur ipsum
+              ducimus quibusdam quis voluptatibus.
+            </p>
+          </article>
+        </section>
+        <section>
+          <h4>2009</h4>
+          <article>
+            <h4>First level graduation in Graphic Design</h4>
+            <p>A</p>
+          </article>
+        </section>
+        <section>
+          <h4>2012</h4>
+          <article>
+            <h4>Second level graduation in Graphic Design</h4>
+            <p>Ducimus, aliquam tempore autem itaque et accusantium!</p>
+          </article>
+        </section>
+      </main>
+    );
   }
 }
 
---
+++ src/education/education.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Educaton extends Component {
+  render() {
+    return <h1>111</h1>;
+  }
+}
+
+export default Educaton;
